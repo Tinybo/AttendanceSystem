52e4335ee69e3c346620ab4374c9ef9db05e3b4a
Tinybo <1069792235@qq.com>
2019-04-22 22:55:49 +0800

    完成课程详情页的数据显示。

--- src/page/attendance/courseDetail/index.js
+++ src/page/attendance/courseDetail/index.js
@@ -92,13 +92,6 @@ class CourseDetail extends Component {
 
     render () {
         const data = this.props.location.query;
-        const text = `
-            A dog is a type of domesticated animal.
-            Known for its loyalty and faithfulness,
-            it can be found as a welcome guest in many households across the world.
-            `;
-
-        console.log('allCourse', data);
 
         return (
             <div className="courseDetailContainer">
